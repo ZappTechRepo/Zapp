4935f3669519424f89d88b7bdb0b9416d600c09f
Kwena <Kwena@warpdevelopment.co.za>
2016-06-01 12:17:28 +0200

    - files

---
+++ Src/Delivery-App/Homemation.WebAPI/TriboschAdmin/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TriboschAdmin.Models
+{
+    public class Contact
+    {
+        public string Email { get; set; }
+        public string Tel { get; set; }
+        public string Mobile { get; set; }
+        public string Fax { get; set; }
+
+        public List<UserAddress> Addresses { get; set; }
+    }
+}
